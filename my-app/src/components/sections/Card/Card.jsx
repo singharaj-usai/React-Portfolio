b47be407d8998e6828c9086d15f2e21cfe14bdff
singharaj usai <djjd22938@gmail.com>
2022-10-15 00:52:09 -0400

    tailwind npm fix

--- my-app/src/components/sections/Card/Card.jsx
+++ my-app/src/components/sections/Card/Card.jsx
@@ -26,7 +26,23 @@ const Card = () => {
             <div>
             <img src={picture} alt="singharaj" className="w-full" />
             </div>
-            
+            <div className="text-center">
+        <h1 className="text-xl text-gray-800 font-bold mb-1">John Doe</h1>
+        <p className="text-sm text-gray-400 mb-3">
+          Frontend Web Developer at
+          <a href="#0" className="text-purple-600 pl-1">
+            Abc Company
+          </a>
+        </p>
+        <a
+          href="#0"
+          className="inline-block mb-3 rounded bg-purple-600 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide hover:bg-purple-800"
+          download="Resume"
+        >
+          Download Resume
+        </a>
+
+      </div>
         </aside>
     );
 };
