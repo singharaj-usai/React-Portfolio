92c1154740644e34dcd621c05a5a743b260d9cfc
singharaj usai <djjd22938@gmail.com>
2022-10-15 01:18:24 -0400

    proper formatting

--- my-app/src/components/sections/Card/Card.jsx
+++ my-app/src/components/sections/Card/Card.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
-import{ FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 import picture from '../../../images/noob.png';
 
 const socials = [
-    {
-        id: 1,
-        icon: <FaGithub/>,
-        link: '#https://github.com/singharaj-usai',
-    },
-    {
-        id: 2,
-        icon: <FaLinkedinIn/>,
-        link: '#https://www.linkedin.com/in/singharaj-usai',
-    },
-    {
-        id: 3,
-        icon: <FaTwitter/>,
-        link: '#https://twitter.com/OMrbobbilly',
-    },
+  {
+    id: 1,
+    icon: <FaGithub />,
+    link: '#https://github.com/singharaj-usai',
+  },
+  {
+    id: 2,
+    icon: <FaLinkedinIn />,
+    link: '#https://www.linkedin.com/in/singharaj-usai',
+  },
+  {
+    id: 3,
+    icon: <FaTwitter />,
+    link: '#https://twitter.com/OMrbobbilly',
+  },
 ]
 
 const Card = () => {
-    return (
-        <aside className="sticky top-0 bg-white md:mx-8 lg:mx-4 mb-8 p-6 shadow-md rounded-md -mt-40">
-            <div className="w-24 h-24 rounded-md overflow-hidden mx-auto mb-5">
-            <img src={picture} alt="singharaj" className="w-full" />
-            </div>
-            <div className="text-center">
+  return (
+    <aside className="sticky top-0 bg-white md:mx-8 lg:mx-4 mb-8 p-6 shadow-md rounded-md -mt-40">
+      <div className="w-24 h-24 rounded-md overflow-hidden mx-auto mb-5">
+        <img src={picture} alt="singharaj" className="w-full" />
+      </div>
+      <div className="text-center">
         <h1 className="text-xl text-gray-800 font-bold mb-1">Singharaj Usai</h1>
         <p className="text-sm text-gray-400 mb-3">
           Fullstack Web Developer Student at
@@ -47,21 +47,21 @@ const Card = () => {
           ))}
         </ul>
       </div>
-        </aside>
-    );
+    </aside>
+  );
 };
-
-
-export default Card;
 
 // From our React class notes props
 const SocialIcon = (props) => {
-    const {icon, link} = props.social;
-    return (
-        <li className='m-2'>
-            <a href={link} className='w-8 h-8 bg-teal-100 rounded text-teal-800 flex items-center justify-center hover:text-white hover:bg-teal-600 transition ease-out duration-200'>
-                {icon}
-                </a>
-        </li>
-    )
+  const { icon, link } = props.social;
+  return (
+    <li className='m-2'>
+      <a href={link} className='w-8 h-8 bg-teal-100 rounded text-teal-800 flex items-center justify-center hover:text-white hover:bg-teal-600 transition ease-out duration-200'>
+        {icon}
+      </a>
+    </li>
+  )
 }
+
+export default Card;
+
