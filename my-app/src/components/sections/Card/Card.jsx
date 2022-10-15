d1272ec1d9ce6b2e12d691d10124c8519032d547
singharaj usai <djjd22938@gmail.com>
2022-10-15 00:06:14 -0400

    social media links

--- my-app/src/components/sections/Card/Card.jsx
+++ my-app/src/components/sections/Card/Card.jsx
@@ -1,3 +1,21 @@
 import React from 'react';
 import{ FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 import picture from '../../../images/roblox noob render.png';
+
+const socials = [
+    {
+        id: 1,
+        icon: <FaGithub/>,
+        link: '#https://github.com/singharaj-usai',
+    },
+    {
+        id: 2,
+        icon: <FaLinkedinIn/>,
+        link: '#https://www.linkedin.com/in/singharaj-usai',
+    },
+    {
+        id: 3,
+        icon: <FaTwitter/>,
+        link: '#https://twitter.com/OMrbobbilly',
+    },
+]
