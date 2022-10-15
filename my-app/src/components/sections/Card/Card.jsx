843e64687ad35da01de138b2750639665ab243b9
singharaj usai <djjd22938@gmail.com>
2022-10-15 00:57:47 -0400

    card template but it looks good!!!

--- my-app/src/components/sections/Card/Card.jsx
+++ my-app/src/components/sections/Card/Card.jsx
@@ -22,21 +22,21 @@ const socials = [
 
 const Card = () => {
     return (
-        <aside >
-            <div>
+        <aside className="sticky top-0 bg-white md:mx-8 lg:mx-4 mb-8 p-6 shadow-md rounded-md -mt-40">
+            <div className="w-24 h-24 rounded-md overflow-hidden mx-auto mb-5">
             <img src={picture} alt="singharaj" className="w-full" />
             </div>
             <div className="text-center">
-        <h1 className="text-xl text-gray-800 font-bold mb-1">John Doe</h1>
+        <h1 className="text-xl text-gray-800 font-bold mb-1">Singharaj Usai</h1>
         <p className="text-sm text-gray-400 mb-3">
-          Frontend Web Developer at
-          <a href="#0" className="text-purple-600 pl-1">
-            Abc Company
+          Fullstack Web Developer Student at
+          <a href="#0" className="text-teal-500 pl-1">
+            Michigan State University's Code Camp
           </a>
         </p>
         <a
           href="#0"
-          className="inline-block mb-3 rounded bg-purple-600 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide hover:bg-purple-800"
+          className="inline-block mb-3 rounded bg-teal-500 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide hover:bg-teal-800"
           download="Resume"
         >
           Download Resume
