e281290638c9760d300e14a9869df7b7d5e9ae0f
singharaj usai <djjd22938@gmail.com>
2022-10-15 01:39:59 -0400

    page routes

---
+++ my-app/src/components/sections/NavBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
+import About from '../../pages/About/About';
+
+const NavBarData = [
+    {
+        id: 1,
+        title: 'About',
+        to: '/about',
+    }
+]
