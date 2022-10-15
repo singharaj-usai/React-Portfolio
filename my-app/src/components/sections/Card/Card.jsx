fe2e1063fe9c49c280f94e30806975ec74ae0a9f
singharaj usai <djjd22938@gmail.com>
2022-10-15 04:20:33 -0400

    fix react stuff

--- my-app/src/components/sections/Card/Card.jsx
+++ my-app/src/components/sections/Card/Card.jsx
@@ -35,13 +35,13 @@ const Card = () => {
         <h1 className="text-xl text-gray-800 font-bold mb-1">Singharaj Usai</h1>
         <p className="text-sm text-gray-400 mb-3">
           Fullstack Web Developer Student at
-          <a href="#https://bootcamp.msu.edu/" className="text-teal-500 pl-1 hover:text-teal-300 transition ease-out duration-200">
+          <a href="#https://bootcamp.msu.edu/" className="text-blue-500 pl-1 hover:text-blue-300 transition ease-out duration-200">
             Michigan State University's Code Camp
           </a>
         </p>
         <a
           href="#0"
-          className="inline-block mb-3 rounded bg-teal-500 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide hover:bg-teal-300 transition ease-out duration-200"
+          className="inline-block mb-3 rounded bg-blue-500 text-center border-0 py-2 px-6 text-white leading-7 tracking-wide hover:bg-blue-300 transition ease-out duration-200"
           download="Resume"
         >
           Download Resume
@@ -61,7 +61,7 @@ const SocialIcon = (props) => {
   const { icon, link } = props.social;
   return (
     <li className='m-2'>
-      <a href={link} className='w-8 h-8 bg-teal-100 rounded text-teal-800 flex items-center justify-center hover:text-white hover:bg-teal-600 transition ease-out duration-200'>
+      <a href={link} className='w-8 h-8 bg-blue-100 rounded text-blue-800 flex items-center justify-center hover:text-white hover:bg-blue-600 transition ease-out duration-200'>
         {icon}
       </a>
     </li>
